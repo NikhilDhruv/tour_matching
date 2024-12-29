4e5f8470da3b44fe84b4d710f41a7dae68c25d31
Nikhil Dhruv <64217800+NikhilDhruv@users.noreply.github.com>
2024-12-29 16:19:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-import openai
-
-openai.api_key = "your_openai_api_key"
-
-response = openai.Embedding.create(
-    model="text-embedding-ada-002",
-    input="Test input"
-)
-
-print(response['data'][0]['embedding'])
+test_input = "Sample input"
+embedding = api_call(test_input)
+print("Embedding:", embedding)
